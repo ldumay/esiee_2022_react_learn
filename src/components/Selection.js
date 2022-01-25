c44789863a8658f82a750291f036478bdd262735
Loïc Dumay <dumay.famille@gmail.com>
2022-01-25 15:34:55 +0100

    V0.0.14

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,5 +1,16 @@
+import { useEffect } from "react"
+
 function Selection(props){
     
+    // Agit après chaque rendu du DOM
+    useEffect( () => {
+        alert("La sélection est : " + props.selection)
+    }, [props.selection]) 
+    /*
+    |--> Pour : [] -->  permet de faire un "useEffect" qu'au 1 rendu
+    |--> Pour : [selection] -->  permet de faire un "useEffect" sélection un élément
+    */
+
     return(
         <div>
             <br/>
